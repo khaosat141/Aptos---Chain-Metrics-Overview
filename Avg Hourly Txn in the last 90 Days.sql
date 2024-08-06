01a7b03017be043c8c76aab3b23b15823039fc2a
khaosat141 <75438440+khaosat141@users.noreply.github.com>
2024-08-06 10:17:40 +0700

    Create Avg Hourly Txn in the last 90 Days.sql

---
+++ Avg Hourly Txn in the last 90 Days.sql
@@ -0,0 +1,24 @@
+WITH transaction_counts AS (
+  SELECT
+    DATE_TRUNC('hour', block_time) AS Hour, /* Round block timestamp to the nearest day */
+    'aptos' AS network, /* Since the table is for Aptos transactions, setting a static value for network */
+    COUNT(*) AS daily_transaction_count /* Count the number of transactions in this day */
+  FROM aptos.transactions
+  WHERE
+    block_time >= CURRENT_TIMESTAMP - INTERVAL '90' day
+  GROUP BY
+    DATE_TRUNC('hour', block_time) /* Group the transactions by day */
+), average_transaction_count AS (
+  SELECT
+    network,
+    AVG(daily_transaction_count) AS avg_daily_transaction_count /* Calculate the average daily transaction count */
+  FROM transaction_counts
+  GROUP BY
+    network
+)
+SELECT
+  network,
+  avg_daily_transaction_count /* Select the network and its average daily transaction count */
+FROM average_transaction_count
+ORDER BY
+  network
